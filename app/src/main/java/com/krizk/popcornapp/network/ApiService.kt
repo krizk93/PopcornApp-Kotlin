a6230ee123ba1aec44abdb01a661f3e4387fa590
Karim Rizk <33064912+krizk93@users.noreply.github.com>
2021-05-18 04:43:55 +0200

    [ADD] Retrofit, API interface and data class

---
+++ app/src/main/java/com/krizk/popcornapp/network/ApiService.kt
@@ -0,0 +1,36 @@
+package com.krizk.popcornapp.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
+private const val BASE_URL = "https://api.themoviedb.org/"
+
+
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+
+private val retrofit =
+    Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(
+        BASE_URL
+    ).build()
+
+
+interface ApiService {
+    @GET("/3/movie/{category}")
+    fun getMovies(@Path("category") category: String, @Query("api_key") apiKey: String):
+            Call<Movies>
+}
+
+object MoviesApi {
+
+    val retrofitService: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
